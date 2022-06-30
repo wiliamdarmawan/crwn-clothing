5c4bd939517db72c7982a4007039308f1c03f932
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-06-30 14:14:10 +0700

    Lesson 3(2)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
@@ -24,15 +26,15 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="background-image" />
-        {categories.map(({title}) => (
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
