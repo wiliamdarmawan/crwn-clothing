0a5de8f732a022a3a5c5c52eb97e9e6c9cffe17d
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-07-18 17:07:16 +0700

    Fixing clashing styles

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Checkout from "./routes/checkout/checkout.component";
 const App = () => {
   return (
     <Routes>
-    <Route path='/' element = {<Navigation/>}>
-      <Route index element={<Home />}/>
-      <Route path="shop" element={<Shop />} />
-      <Route path="auth" element={<Authentication />} />
-      <Route path="checkout" element={<Checkout/>} />
-    </Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
     </Routes>
   );
 };
