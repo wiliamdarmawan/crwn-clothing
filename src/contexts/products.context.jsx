9d6cab4df9ae3687d6822585343a1c016eba69e3
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-07-18 14:51:03 +0700

    Get Products + Categories from Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,16 +1,29 @@
-import { createContext, useState } from "react";
-import SHOP_DATA from '../shop-data.js';
+import { createContext, useState, useEffect } from "react";
+import {
+  getCategoriesAndDocuments,
+} from "../utils/firebase/firebase.utils.js";
 
 export const ProductContext = createContext({
   products: [],
-  setProducts: () => null,
 });
 
 export const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const value = { products, setProducts };
 
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+
+    getCategoriesMap();
+  });
+
   return (
-    <ProductContext.Provider value={value}> {children} </ProductContext.Provider>
+    <ProductContext.Provider value={value}>
+      {" "}
+      {children}{" "}
+    </ProductContext.Provider>
   );
 };
